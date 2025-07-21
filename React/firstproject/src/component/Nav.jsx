ed9632ae5ff060c69fb4c9d52b2205c6fa33685e
Atharva <atharvawork.3@gmail.com>
2025-07-21 19:43:59 +0530

    Creating Navbar in React

---
+++ React/firstproject/src/component/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+function Nav() {
+  return (
+    <div className='navbar'>
+        <div className='logo'></div>
+        <div className='menu'></div>
+    </div>
+  )
+}
+
+export default Nav
