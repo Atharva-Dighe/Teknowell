2fef11c2dd46666535a19915804275e90332d40c
Atharva <atharvawork.3@gmail.com>
2025-07-21 19:54:57 +0530

    Updated Navbar Code & CSS

--- React/firstproject/src/component/Nav.jsx
+++ React/firstproject/src/component/Nav.jsx
@@ -3,8 +3,14 @@ import './Nav.css'
 function Nav() {
   return (
     <div className='navbar'>
-        <div className='logo'></div>
-        <div className='menu'></div>
+        <div className='logo'>
+          <img src="https://www.teknowell.in/assets/img/Teknowell%20logo-new.webp" className='logo-img' alt="Logo" />
+        </div>
+        <div className='menu'>
+          <p><a href="">Home</a></p>
+          <p><a href="">About</a></p>
+          <p><a href="">Contact</a></p>
+        </div>
     </div>
   )
 }
