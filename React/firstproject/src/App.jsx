d8ab607f814f68c1d1fdbe8b4ec160db39fab21c
Atharva <atharvawork.3@gmail.com>
2025-07-21 19:39:22 +0530

    Adding React folder & creating firstproject

---
+++ React/firstproject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './component/Nav'
+import Home from './component/Home'
+
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
