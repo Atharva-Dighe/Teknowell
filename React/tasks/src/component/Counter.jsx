a331e7deb879008d58104ef6ed89a7e703623d52
Atharva <atharvawork.3@gmail.com>
2025-07-23 19:19:03 +0530

    Adding Counter project

---
+++ React/tasks/src/component/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './Counter.css'
+
+function Counter() {
+    const [value, setValue] = useState(1);
+    const price=100;
+    function decrement()
+    {
+        setValue(value>1 ? value - 1 : 1);
+    }
+    function increment()
+    {
+        setValue(value + 1);
+    }
+    return (
+        <div className='div'>
+            <div className='valueDiv'>
+                <button onClick={decrement} className='button'>-</button>
+                <h1 className='value'>{value}</h1>
+                <button onClick={increment} className='button'>+</button>
+            </div>
+            <div className='priceDiv'>
+                <h1>{price*value}</h1>
+            </div>
+        </div>
+    )
+}
+export default Counter
